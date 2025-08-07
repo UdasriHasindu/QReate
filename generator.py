4485ef32b75d5192eec4a3ba4c4da63970222934
UdasriHasindu <usrimart@gmail.com>
2025-08-07 21:52:26 +0530

    implement logics to generate QR codes

---
+++ generator.py
@@ -0,0 +1,25 @@
+import qrcode
+import uuid
+import os
+
+
+def Generate_QR(data):
+
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    img_uid = uuid.uuid4().hex[:12]
+    file_name = f"{img_uid}.png"
+    file_path = os.path.join("static", "qrcodes", file_name)
+
+    img.save(file_path)
+    return img_uid
+
