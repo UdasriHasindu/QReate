7069b4d2c988e253a4f2ca355ee66f386e867ef5
UdasriHasindu <usrimart@gmail.com>
2025-08-08 10:25:15 +0530

    ensure qrcodes directory exists

--- generator.py
+++ generator.py
@@ -5,6 +5,10 @@ import os
 
 def Generate_QR(data):
 
+    # check if qrcodes directory exists
+    qr_dir = os.path.join("static", "qrcodes")
+    os.makedirs(qr_dir, exist_ok=True)
+
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -16,9 +20,10 @@ def Generate_QR(data):
 
     img = qr.make_image(fill_color="black", back_color="white")
 
+    # generate file path
     img_uid = uuid.uuid4().hex[:12]
     file_name = f"{img_uid}.png"
-    file_path = os.path.join("static", "qrcodes", file_name)
+    file_path = os.path.join(qr_dir, file_name)
 
     img.save(file_path)
     return img_uid
